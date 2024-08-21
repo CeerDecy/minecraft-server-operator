a6dc5bfb569ba28636ae7c841758d9579345de5e
CeerDecy <1748788674@qq.com>
2024-08-21 14:02:17 +0800

    minecraft server operator

---
+++ api/v1/mcserver_types.go
@@ -0,0 +1,82 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// McServerSpec defines the desired state of McServer
+type McServerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Image string `json:"image,omitempty"`
+
+	Version string `json:"version,omitempty"`
+
+	StorageSize resource.Quantity `json:"storageSize,omitempty"`
+
+	StorageClassName string `json:"storageClassName,omitempty"`
+
+	Resources corev1.ResourceRequirements `json:"resources,omitempty" protobuf:"bytes,8,opt,name=resources"`
+}
+
+// McServerStatus defines the observed state of McServer
+type McServerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Version string `json:"version,omitempty"`
+	State   string `json:"state,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// McServer is the Schema for the mcservers API
+type McServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   McServerSpec   `json:"spec,omitempty"`
+	Status McServerStatus `json:"status,omitempty"`
+}
+
+func (m *McServer) NewLabels() map[string]string {
+	return map[string]string{
+		"app.kubernetes.io/component": "mcserver",
+		"app.kubernetes.io/name":      m.Name,
+		"app.kubernetes.io/version":   m.Status.Version,
+	}
+}
+
+// +kubebuilder:object:root=true
+
+// McServerList contains a list of McServer
+type McServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []McServer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&McServer{}, &McServerList{})
+}
---
+++ internal/controller/mcserver_controller.go
@@ -0,0 +1,191 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	minecraftserverv1 "ceerdecy.com/minecraft-server-operator/api/v1"
+)
+
+var PersistentVolumeFilesystem = corev1.PersistentVolumeFilesystem
+
+// McServerReconciler reconciles a McServer object
+type McServerReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=minecraft-server.ceerdecy.com,resources=mcservers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=minecraft-server.ceerdecy.com,resources=mcservers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=minecraft-server.ceerdecy.com,resources=mcservers/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the McServer object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.4/pkg/reconcile
+func (r *McServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	logger.Info("start reconcile")
+
+	var mcServer minecraftserverv1.McServer
+	var result ctrl.Result
+
+	err := r.Get(ctx, req.NamespacedName, &mcServer)
+	if err != nil {
+		logger.Error(err, "failed to get mcServer")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	mcServer.Status.Version = mcServer.Spec.Version
+	mcServer.Status.State = "Running"
+
+	err = r.Status().Update(ctx, &mcServer)
+	if err != nil {
+		logger.Error(err, "failed to get mcServer")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      mcServer.Name,
+			Namespace: mcServer.Namespace,
+		},
+	}
+
+	_, err = ctrl.CreateOrUpdate(ctx, r.Client, sts, func() error {
+		buildStatefulSet(&mcServer, sts)
+		return ctrl.SetControllerReference(&mcServer, sts, r.Scheme)
+	})
+
+	svc := buildService(&mcServer)
+
+	_, err = ctrl.CreateOrUpdate(ctx, r.Client, svc, func() error {
+		return ctrl.SetControllerReference(&mcServer, svc, r.Scheme)
+	})
+
+	logger.Info("end reconcile")
+
+	return result, nil
+}
+
+func buildService(mc *minecraftserverv1.McServer) *corev1.Service {
+	var svc corev1.Service
+
+	return &svc
+}
+
+func buildStatefulSet(mc *minecraftserverv1.McServer, sts *appsv1.StatefulSet) *appsv1.StatefulSet {
+	labels := mc.NewLabels()
+	podLabels := make(map[string]string, len(labels)+len(mc.Labels))
+
+	for k, v := range mc.Labels {
+		podLabels[k] = v
+	}
+
+	sts.Spec = appsv1.StatefulSetSpec{
+		ServiceName: mc.Name,
+		Replicas:    ptr.To(int32(1)),
+		Selector: &metav1.LabelSelector{
+			MatchLabels: mc.Labels,
+		},
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: mc.Annotations,
+				Labels:      podLabels,
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name:      "mc-server",
+						Image:     mc.Spec.Image,
+						Resources: mc.Spec.Resources,
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "world",
+								MountPath: "/server/world",
+							},
+						},
+						Ports: []corev1.ContainerPort{
+							{ContainerPort: 25565},
+						},
+					},
+					{
+						Name: "mc-status",
+						Resources: corev1.ResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceCPU:    resource.MustParse("100m"),
+								corev1.ResourceMemory: resource.MustParse("100Mi"),
+							},
+						},
+						Image: "registry.cn-hangzhou.aliyuncs.com/ceerdecy/minecraft-server:statuser-1.0.0",
+						Ports: []corev1.ContainerPort{
+							{ContainerPort: 25566},
+						},
+					},
+				},
+			},
+		},
+		VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "world",
+					Namespace: mc.Namespace,
+					Labels:    labels,
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.VolumeResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: mc.Spec.StorageSize,
+						},
+					},
+					StorageClassName: &mc.Spec.StorageClassName,
+					VolumeMode:       &PersistentVolumeFilesystem,
+				},
+			},
+		},
+	}
+
+	return sts
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *McServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&minecraftserverv1.McServer{}).
+		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.Service{}).
+		Complete(r)
+}
